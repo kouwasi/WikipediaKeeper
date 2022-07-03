20f849bdd3faf27de20801b4e52c8bf7f0743ce3
kouwasi <kouwasi@gmail.com>
2022-07-04 02:43:24 +0900

    ..

--- js/index.js
+++ js/index.js
@@ -3,6 +3,6 @@ document.querySelectorAll('.mw-workspace-container').forEach((el) => {
     el.classList.remove('mw-workspace-container');
 });
 document.querySelector('.mw-article-toolbar-container').style.maxWidth = 'none';
-document.querySelectorAll('.mw-content-container').forEach((el => {
+document.querySelectorAll('.mw-content-container').forEach((el) => {
     el.style.maxWidth = 'none';
-}))
+})
